342f8c995b77bb7410f4fbb447b2b950101605c5
divyesh123-jain <97737499+divyesh123-jain@users.noreply.github.com>
2023-02-03 17:52:51 +0530

    error

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,20 @@
+import { shuffleArray } from "./utils";
+
+
+export type Question = {
+        category: string;
+        correct_answer: string;
+        difficulty:string;
+        incorrect_answer:string[];
+        question:string;
+        type:string;
+
+};
+
+
+export type  QuestionState = Question & {answer : string []};
+
+
 
 export enum Difficulty {
 
@@ -12,5 +29,18 @@ export enum Difficulty {
 
 
 
-export const fetchQuizQuestions = async(amount : number , difficulty : Difficulty )
+export const fetchQuizQuestions = async(amount : number , difficulty : Difficulty ) => {
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`
+    const data = await (await fetch(endpoint)).json();
+    return data.results.map((question: Question) => (
+        {
+            ...question,
+            answer:  shuffleArray([...question.incorrect_answer, question.correct_answer])
+        }
+    ))
+    };
+
+
+
+
     
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,48 @@
 import React , {useState, useTransition} from 'react';
 import Cards from './components/Cards';
+import { fetchQuizQuestions } from './API';
+import {QuestionState , Difficulty } from './API';
+
+
+
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
 
 const [loading , setLoading] = useState(false);
-const [questions , setQuestions ] = useState([]);
-const [userAnswers , setUserAnswers ] = useState([]);
+const [questions , setQuestions ] = useState<QuestionState[]>([]);
+const [userAnswers , setUserAnswers ] = useState<AnswerObject[]>([]);
 const [number , setNumber] = useState(0);
 const [score , setScore ] = useState(0);
 const [gameOver ,  setGameOver] = useState(true);
 
-
-
+console.log(questions);
+// console.log(fetchQuizQuestions(TOTAL_QUESTIONS , Difficulty.EASY));
 
 const startTrivia  = async () => {
+
+  setLoading(true);
+  setGameOver(false);
+
+
+const newQuestions = await  fetchQuizQuestions(
+  TOTAL_QUESTIONS,
+  Difficulty.EASY
+);
+
+setQuestions(newQuestions);
+  setScore(0);
+  setUserAnswers([]);
+  setNumber(0);
+  setLoading(false);
 
 }
 
@@ -35,14 +62,14 @@ const nextQuestion = () => {
       </button>
       <p className='' >SCORE:</p>
       <p className=''>LOADING QUESTIONS ....</p>
-      <Cards 
+      {/* <Cards 
       questionNr={number + 1}
       totalQuestions={TOTAL_QUESTIONS}
       question={questions[number].question}
       answers={questions[number].answers}
       userAnswer={userAnswers ? userAnswers[number] : undefined}
       callback={checkAnswer}
-      />
+      /> */}
       
       <button className='' onClick={nextQuestion}>
         NEXT QUESTION
